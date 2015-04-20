064e72e6ce647fb2800f21544b0beb66d3da5622
Adrian <adrioter94@hotmail.es>
2015-04-20 12:18:47 +0200

    Entrega Ejercicio

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'acortador_urls.views.Formulario'),
+    url(r'^(\d*)$', 'acortador_urls.views.Busqueda'),
+)
